0132672895016a79c71ff07a6adca301692cf39f
kamila <58568615+kamilarakhimova@users.noreply.github.com>
2023-04-10 22:19:13 +0000

    Create algorithms.h

---
+++ algorithms.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::ios_base;
+using std::string;
+
+int naive(string& pattern, string& text);
+int kmp1(string& pattern, string& text);
+int kmp2(string& pattern, string& text);
+int kmp3(string& pattern, string& text);
