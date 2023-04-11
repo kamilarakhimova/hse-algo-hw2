fe7821300dbda04f2e632d3306428f384d7ee222
kamila <58568615+kamilarakhimova@users.noreply.github.com>
2023-04-11 20:48:38 +0000

    Update algorithms.h

--- algorithms.h
+++ algorithms.h
@@ -7,6 +7,10 @@ using std::ios_base;
 using std::string;
 
 int naive(string& pattern, string& text);
+int naive_mode(string& pattern, string& text);
 int kmp1(string& pattern, string& text);
+int kmp1_mode(string& pattern, string& text);
 int kmp2(string& pattern, string& text);
-int kmp3(string& pattern, string& text);
+int kmp2_mode(string& pattern, string& text);
+int bmh(string& pattern, string& text);
+int bmh_mode(string& pattern, string& text);
